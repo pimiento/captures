4ce3fea767f6fc07d703617be1f4c419b7348af8
Vavilin <shtartora@gmail.com>
2013-11-20 16:44:47 +0600

    Mouse captures produce core dumpes

--- browser/camera.py
+++ browser/camera.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import time
 import tempfile
@@ -5,11 +6,6 @@ import cv2.cv as cv
 
 __all__ = ["take_picture",]
 FLAG = False
-
-def on_mouse(event, x, y, flag, param):
-    global FLAG
-    if event == cv.CV_EVENT_LBUTTONDOWN:
-        FLAG = True
 
 def take_picture():
     cv.NamedWindow("camera", 1)
@@ -20,7 +16,6 @@ def take_picture():
     while True:
         img = cv.QueryFrame(capture)
         cv.ShowImage("camera", img)
-        cv.SetMouseCallback("camera", on_mouse)
         if cv.WaitKey(10) in [10, 27, 32] or FLAG:
             cv.SaveImage(filename, img)
             break
